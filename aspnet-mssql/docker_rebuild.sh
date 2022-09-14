b6e14bee6a23b581de49d0b7ff02765eb7670566
samuel camargo de souza <samuelsouza@samuels-macbook-pro.home>
2022-09-14 23:02:30 +0200

    new config

--- aspnet-mssql/docker_rebuild.sh
+++ aspnet-mssql/docker_rebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Rebuild
-docker compose build
-docker compose up -d --force-recreate
+docker compose -f compose-local.yaml build 
+docker compose -f compose-local.yaml up -d --force-recreate
 
 # Run Test suite
 sleep 10
